90663ab1c74fd4d09fa152991457faa8e6ebc45d
Tanisha Majmundar <majmundartanisha@gmail.com>
2026-02-13 20:43:04 +0530

    Add user validation middleware for create and update

--- routes/userroute.js
+++ routes/userroute.js
@@ -3,10 +3,22 @@ const router = express.Router();
 
 const userController = require('../controllers/usercontroller');
 
+function validateUser(req, res, next) {
+    const { name, email } = req.body;
+
+    if (!name || !email) {
+        return res.status(400).json({
+            message: "Invalid user data"
+        });
+    }
+
+    next();
+}
+
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.getUserById);
-router.post('/', userController.createUser);
-router.put('/:id', userController.updateUser);
+router.post('/', validateUser, userController.createUser);
+router.put('/:id', validateUser, userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
