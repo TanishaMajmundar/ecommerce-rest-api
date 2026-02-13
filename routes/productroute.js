5f0a0b965b365c719f8ec7c5a38b5e8ebc27113e
Tanisha Majmundar <majmundartanisha@gmail.com>
2026-02-13 20:35:55 +0530

    Add validation for product creation and updates

--- routes/productroute.js
+++ routes/productroute.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
-
 const productController = require('../controllers/productcontroller');
+
+function validateProduct(req, res, next) {
+    const { name, price } = req.body;
+
+    if (!name || typeof price !== 'number') {
+        return res.status(400).json({
+            message: "Invalid product data"
+        });
+    }
+
+    next();
+}
 
 router.get('/', productController.getAllProducts);
 router.get('/:id', productController.getProductById);
-router.post('/', productController.createProduct);
-router.put('/:id', productController.updateProduct);
+router.post('/', validateProduct, productController.createProduct);
+router.put('/:id', validateProduct, productController.updateProduct);
 router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
