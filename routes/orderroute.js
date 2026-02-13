635a946ccfed9012c0f3450cd584f579a2ed1adf
Tanisha Majmundar <majmundartanisha@gmail.com>
2026-02-13 20:51:29 +0530

    Add order validation middleware for create and update

--- routes/orderroute.js
+++ routes/orderroute.js
@@ -3,10 +3,20 @@ const router = express.Router();
 
 const orderController = require('../controllers/ordercontroller');
 
+function validateOrder(req, res, next) {
+    const { userId, total } = req.body;
+    if (!userId || typeof total !== 'number') {
+        return res.status(400).json({
+            message: "Invalid order data"
+        });
+    }
+    next();
+}
+
 router.get('/', orderController.getAllOrders);
 router.get('/:id', orderController.getOrderById);
-router.post('/', orderController.createOrder);
-router.put('/:id', orderController.updateOrder);
+router.post('/', validateOrder, orderController.createOrder);
+router.put('/:id', validateOrder, orderController.updateOrder);
 router.delete('/:id', orderController.deleteOrder);
 
 module.exports = router;
