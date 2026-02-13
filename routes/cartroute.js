a746026ad264f42ff58376691f3d41bbc99e8826
Tanisha Majmundar <majmundartanisha@gmail.com>
2026-02-13 20:47:24 +0530

    Implement cart data validation middleware
    Added validation middleware for cart item data before adding or updating.

--- routes/cartroute.js
+++ routes/cartroute.js
@@ -3,10 +3,20 @@ const router = express.Router();
 
 const cartController = require('../controllers/cartcontroller');
 
+function validateCart(req, res, next) {
+    const { productId, quantity } = req.body;
+    if (!productId || typeof quantity !== 'number') {
+        return res.status(400).json({
+            message: "Invalid cart data"
+        });
+    }
+    next();
+}
+
 router.get('/', cartController.getCartItems);
 router.get('/:id', cartController.getCartItemById);
-router.post('/', cartController.addCartItem);
-router.put('/:id', cartController.updateCartItem);
+router.post('/', validateCart, cartController.addCartItem);
+router.put('/:id', validateCart, cartController.updateCartItem);
 router.delete('/:id', cartController.deleteCartItem);
 
 module.exports = router;
