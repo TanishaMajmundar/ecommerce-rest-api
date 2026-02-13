951d52ddd826314bf4cb9040b594e83f6928ff40
Tanisha Majmundar <majmundartanisha@gmail.com>
2026-02-13 20:40:25 +0530

    Update usercontroller.js

--- controllers/usercontroller.js
+++ controllers/usercontroller.js
@@ -3,12 +3,10 @@ let users = [
   { id: 2, name: 'Jahanvi', email: 'jahanvi@email.com' }
 ];
 
-// GET /users
 exports.getAllUsers = (req, res) => {
   res.json(users);
 };
 
-// GET /users/:id
 exports.getUserById = (req, res) => {
   const id = parseInt(req.params.id);
   const user = users.find(u => u.id === id);
@@ -20,7 +18,6 @@ exports.getUserById = (req, res) => {
   res.json(user);
 };
 
-// POST /users
 exports.createUser = (req, res) => {
   const newUser = {
     id: users.length + 1,
@@ -32,7 +29,6 @@ exports.createUser = (req, res) => {
   res.status(201).json(newUser);
 };
 
-// PUT /users/:id
 exports.updateUser = (req, res) => {
   const id = parseInt(req.params.id);
   const user = users.find(u => u.id === id);
@@ -47,7 +43,6 @@ exports.updateUser = (req, res) => {
   res.json(user);
 };
 
-// DELETE /users/:id
 exports.deleteUser = (req, res) => {
   const id = parseInt(req.params.id);
   users = users.filter(u => u.id !== id);
